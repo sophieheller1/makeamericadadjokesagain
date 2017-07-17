4d8a91931f46a2813f405640a9aa87cdcc2d8557
Sophie Heller <sophieheller1@users.noreply.github.com>
2017-07-17 15:31:38 -0400

    Correctly grabs all elements to be mutated

--- dadjokes.js
+++ dadjokes.js
@@ -2,11 +2,13 @@
 var tweets = document.querySelectorAll('twitterwidget');
 for (var i = 0; i < tweets.length; i++) {
 	var username = tweets[i].shadowRoot.querySelector('.TweetAuthor-screenName')
-	var name = tweets[0].shadowRoot.querySelector('.TweetAuthor-name')
+	var authorName = tweets[i].shadowRoot.querySelector('.TweetAuthor-name')
+	var avatar = tweets[i].shadowRoot.querySelector('.Avatar')
 
 	if (username && username.textContent === '@realDonaldTrump') {
 		username.textContent = '@realDadJokes'
-		name.textContent 'Dad J. Joke'
+		authorName.textContent = 'Dad J. Joke'
+		avatar.src = 'icon.png'
 		var trumpTweet = tweets[i].shadowRoot.querySelector('.Tweet-text')
 		if (trumpTweet) {
 			trumpTweet.innerText = 'Dad Joke here'
